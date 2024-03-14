a36940b77996dcd3ccf5a25b8e634ba709d56a39
Jun Anthony Calupcupan <junanthony.business@gmail.com>
2024-03-14 22:51:03 +1300

    Add Customer and Issue Model

---
+++ RepositoryPractice3/Models/tblCustomer.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RepositoryPractice3.Models
+{
+    public class tblCustomer
+    {
+        [Key]
+        public int CustomerId { get; set; }
+        [Required]
+        public string CustomerName { get; set; }
+    }
+}
---
+++ RepositoryPractice3/Models/tblIssue.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RepositoryPractice3.Models
+{
+    public class tblIssue
+    {
+        [Key]
+        public int IssueId { get; set; }
+        [Required]
+        public string Problem { get; set; }
+        [Required]
+        public string CarMake { get; set; }
+        public string CarModel { get; set; }
+        [Required]
+        public string CarRego { get; set; }
+        public string PaymentStatus { get; set; }
+        public string WorkStatus { get; set; }
+    }
+}

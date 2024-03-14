079c0471944ab4f44090ef2a7374b78e7c7b94a5
Jun Anthony Calupcupan <junanthony.business@gmail.com>
2024-03-14 23:17:49 +1300

    Add CustomerList Controller and View Index

---
+++ RepositoryPractice3/Controllers/CustomerListController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RepositoryPractice3.Repository.Irepository;
+
+namespace RepositoryPractice3.Controllers
+{
+    public class CustomerListController : Controller
+    {
+        private readonly ICustomerRepository _customerRepository;
+        public CustomerListController(ICustomerRepository customerRepository)
+        {
+            _customerRepository = customerRepository;
+        }
+        public IActionResult Index()
+        {
+            ViewBag.CustomerListWithIssue = _customerRepository.GetCustomer();
+            return View();
+        }
+    }
+}

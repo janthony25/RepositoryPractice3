852dbb32b4a616c1edba7334a8828a3ce893625a
Jun Anthony Calupcupan <junanthony.business@gmail.com>
2024-03-14 23:07:17 +1300

    Update ApplicationDbContext

--- RepositoryPractice3/Data/ApplicationDbContext.cs
+++ RepositoryPractice3/Data/ApplicationDbContext.cs
@@ -12,5 +12,21 @@ namespace RepositoryPractice3.Data
 
         public DbSet<tblCustomer> tblCustomer { get; set; }
         public DbSet<tblIssue> tblIssue { get; set; }
+        public DbSet<VewTblCustomerWithIssue> VewTblCustomerWithIssue { get; set}
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<VewTblCustomerWithIssue>(Entity =>
+            {
+                Entity.HasNoKey().ToView("VewTblCustomerWithIssue");
+                Entity.Property(e => e.CustomerId).HasColumnName("CustomerId");
+                Entity.Property(e => e.CustomerName).HasMaxLength(50).HasColumnName("CustomerName");
+                Entity.Property(e => e.IssueId).HasColumnName("IssueId");
+                Entity.Property(e => e.Problem).HasColumnName("Problem");
+                Entity.Property(e => e.PaymentStatus).HasColumnName("PaymentStatus");
+                Entity.Property(e => e.WorkStatus).HasColumnName("WorkStatus");
+            }); 
+            base.OnModelCreating(modelBuilder);
+        }
     }
 }
